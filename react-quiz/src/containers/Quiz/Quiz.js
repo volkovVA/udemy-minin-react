b5d671bc5303c8e63c68cf1d23f067446b8c9da9
volkovVA <volkov.slava@hotmail.com>
2021-11-22 11:45:31 +0300

    feat: add active question component

--- react-quiz/src/containers/Quiz/Quiz.js
+++ react-quiz/src/containers/Quiz/Quiz.js
@@ -1,15 +1,21 @@
 import React, { Component } from "react";
 import classes from './Quiz.module.css';
 
+import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz";
+
 export default class Quiz extends Component {
 
   state = {
     quiz: []
   }
+
   render() {
     return (
       <div className={ classes.Quiz }>
-        <h1>Quiz</h1>
+        <div className={ classes.QuizWrapper }>
+          <h1>Quiz</h1>
+          <ActiveQuiz />
+        </div>
       </div>
     );
   }
