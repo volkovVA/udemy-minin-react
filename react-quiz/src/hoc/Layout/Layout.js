35ec3ef1b317f30b8af4357e43f7c1e2002696e2
volkovVA <volkov.slava@hotmail.com>
2021-11-23 23:29:28 +0300

    feat: add menu switch button

--- react-quiz/src/hoc/Layout/Layout.js
+++ react-quiz/src/hoc/Layout/Layout.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 import classes from './Layout.module.css';
 
+import MenuToggle from '../../components/Navigation/MenuToggle/MenuToggle';
+
 export default class Layout extends Component {
+
+  state = {
+    menu: false
+  }
+
+
+  toggleMenuHandler = () => {
+    this.setState({
+      menu: !this.state.menu
+    })
+  }
+
+
   render() {
     return (
       <div className={classes.Layout}>
+
+        <MenuToggle
+          onToggle={ this.toggleMenuHandler }
+          isOpen={ this.state.menu }
+        />
+
         <main>
           { this.props.children }
         </main>
