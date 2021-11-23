6f3461ec621a6a59e419bf148782fd8f956e9fb3
volkovVA <volkov.slava@hotmail.com>
2021-11-23 23:16:40 +0300

    feat: add button component

--- react-quiz/src/components/FinishedQuiz/FinishedQuiz.js
+++ react-quiz/src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,6 +1,8 @@
 import React from "react";
 import classes from './FinishedQuiz.module.css';
 
+import Button from "../UI/Button/Button";
+
 const FinishedQuiz = props => {
   const successCount = Object.keys(props.results).reduce((total, key) => {
     if (props.results[key] === 'success') {
@@ -32,7 +34,8 @@ const FinishedQuiz = props => {
       </ul>
       <p>Правильно { successCount } из { props.quiz.length }</p>
       <div>
-        <button onClick={ props.onRetry }>Повторить</button>
+        <Button onClick={ props.onRetry } type="primary">Повторить</Button>
+        <Button type="success">Перейти в список тестов</Button>
       </div>
     </div>
   )
