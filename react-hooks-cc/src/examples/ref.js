77c3c9702ecdbcf7ae6f9ba7a0bde6aa28881884
volkovVA <volkov.slava@hotmail.com>
2021-12-02 13:36:31 +0300

    feat: add use-ref

---
+++ react-hooks-cc/src/examples/ref.js
@@ -0,0 +1,32 @@
+import React, { useRef, useState, useEffect } from 'react';
+
+function App() {
+
+  // const [renderCount, setRenderCount] = useState(1);
+  const [value, setValue] = useState('initial');
+  const renderCount = useRef(1);
+  const inputRef = useRef(null);
+  const prevValue = useRef('');
+
+  useEffect(() => {
+    renderCount.current++;
+    console.log(inputRef.current);
+  })
+
+  useEffect(() => {
+    prevValue.current = value;
+  }, [value])
+
+  const focus = () => inputRef.current.focus()
+
+  return (
+    <div>
+      <h1>Количество рендеров: {renderCount.current}</h1>
+      <h2>Прошлое состояние: {prevValue.current}</h2>
+      <input ref={inputRef} type="text" onChange={e => setValue(e.target.value)} value={value} />
+      <button className="btn btn-success m-2" onClick={focus}>Фокус</button>
+    </div>
+  );
+}
+
+export default App;
