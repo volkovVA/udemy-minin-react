371dae4257514b1642d36da27d761645fae83e92
volkovVA <volkov.slava@hotmail.com>
2021-12-13 11:19:52 +0300

    feat: add routing and functional components

--- react-router-theory/src/Cars/Car/Car.js
+++ react-router-theory/src/Cars/Car/Car.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import './Car.scss'
+import {withRouter} from 'react-router-dom'
 
 
 const Car = props => {
 
-  console.log(props);
+  // console.log(props);
 
   return (
-    <div className={'Car'}>
+    <div
+      className={'Car'}
+      onClick={() => props.history.push('/cars/' + props.name.toLowerCase())}
+    >
       <h3>Сar name: {props.name}</h3>
       <p>Year: <strong>{props.year}</strong></p>
     </div>
   )
 }
 
-export default Car
+export default withRouter(Car)
