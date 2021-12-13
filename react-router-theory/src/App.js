8a71a7896110e0b3dc7bb4341281e32e25554616
volkovVA <volkov.slava@hotmail.com>
2021-12-13 10:54:00 +0300

    feat: add routing and components

--- react-router-theory/src/App.js
+++ react-router-theory/src/App.js
@@ -17,15 +17,18 @@ class App extends Component {
             <li>
               <a href="/about">About</a>
             </li>
+            <li>
+              <a href="/cars">Cars</a>
+            </li>
           </ul>
         </nav>
 
         <hr/>
 
         <Route path="/" exact render={() => <h1>Home page!!!</h1>} />
-        <About />
+        <Route path="/about" component={About} />
+        <Route path="/cars" component={Cars} />
 
-        <Cars />
       </div>
     );
   }
