c151764f3082e4aa16a205c753623d56371d961f
Volkov Viacheslav <volkov.slava@hotmail.com>
2021-12-20 11:24:18 +0300

    feat: add configuring router & creating pages

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, {Component} from "react";
 import Layout from "./hoc/Layout/Layout";
+import {Route, Routes} from 'react-router-dom';
 
 import Quiz from "./containers/Quiz/Quiz";
+import QuizCreator from "./containers/QuizCreator/QuizCreator";
+import QuizList from "./containers/QuizList/QuizList";
+import Auth from "./containers/Auth/Auth";
 
-function App() {
-  return (
-    <Layout>
-      <Quiz />
-    </Layout>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Layout>
+        <Routes>
+          <Route path="/auth" element={<Auth/>} />
+          <Route path="/quiz/creator" element={<QuizCreator/>} />
+          <Route path="/quiz/:id" element={<Quiz/>} />
+          <Route path="/" element={<QuizList />} />
+        </Routes>
+      </Layout>
+    )
+  }
 }
 
 export default App;
--- react-quiz/src/index.js
+++ react-quiz/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom'
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
+const app = (
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>
+)
+
+ReactDOM.render(
+  app,
   document.getElementById('root')
 );
