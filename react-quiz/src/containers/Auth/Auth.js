8fee0ee78a125deffa9d2e32e4df695639d025d3
Volkov Viacheslav <volkov.slava@hotmail.com>
2021-12-21 17:00:05 +0300

    feat: add login page

--- react-quiz/src/containers/Auth/Auth.js
+++ react-quiz/src/containers/Auth/Auth.js
@@ -1,10 +1,48 @@
 import React, {Component} from "react";
+import Button from '../../components/UI/Button/Button'
+import classes from './Auth.module.css';
 
 export default class Auth extends Component {
+
+  loginHandler = () => {
+
+  }
+
+  registerHandler = () => {
+
+  }
+
+  submitHandler = e => {
+    e.preventDefault();
+  }
+
   render() {
     return (
-      <div>
-        <h1>Auth</h1>
+      <div className={classes.Auth}>
+        <div>
+          <h1>Auth</h1>
+          <form
+            action=""
+            onSubmit={this.submitHandler}
+            className={classes.Authform}
+          >
+            <input type="text" />
+            <input type="text" />
+
+            <Button
+              type="success"
+              onClick={this.loginHandler}
+            >
+              Войти
+            </Button>
+            <Button
+              type="primary"
+              onClick={this.registerHandler}
+            >
+              Зарегистрироваться
+            </Button>
+          </form>
+        </div>
       </div>
     )
   }
