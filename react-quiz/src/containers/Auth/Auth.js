57f79650855235b039d9253608e1ce089422f90c
Volkov Viacheslav <volkov.slava@hotmail.com>
2021-12-22 15:13:11 +0300

    feat: add authorization

--- react-quiz/src/containers/Auth/Auth.js
+++ react-quiz/src/containers/Auth/Auth.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import Button from '../../components/UI/Button/Button';
 import Input from '../../components/UI/Input/Input';
+import axios from 'axios';
 import classes from './Auth.module.css';
 
 const validateEmail = (email) => {
@@ -43,12 +44,32 @@ export default class Auth extends Component {
     }
   }
 
-  loginHandler = () => {
-
+  loginHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true
+    }
+    try {
+      const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA94SDDrwV8c-G2OILFPo_zqv9yNkcZ8hI', authData);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
-  registerHandler = () => {
-
+  registerHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true
+    }
+    try {
+      const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA94SDDrwV8c-G2OILFPo_zqv9yNkcZ8hI', authData);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   submitHandler = e => {
