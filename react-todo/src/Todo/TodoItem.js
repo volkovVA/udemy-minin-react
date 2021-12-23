33a8992488678995bc5c9c77bdbd10c852464258
Volkov Viacheslav <volkov.slava@hotmail.com>
2021-12-23 12:05:02 +0300

    feat: add dynamic classes

--- react-todo/src/Todo/TodoItem.js
+++ react-todo/src/Todo/TodoItem.js
@@ -17,12 +17,19 @@ const styles = {
 }
 
 function TodoItem({ todo, index, onChange }) {
-  console.log(todo);
+
+  const classes = [];
+
+  if (todo.completed) {
+    classes.push('done')
+  }
+
   return (
     <li style={styles.li}>
-      <span>
+      <span className={classes.join(' ')}>
         <input
           type="checkbox"
+          checked={todo.completed}
           style={styles.input}
           onChange={() => onChange(todo.id)}
         />
