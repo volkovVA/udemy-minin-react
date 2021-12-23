a73b48b8a59ac5561ef5ab140f42bd449c723133
Volkov Viacheslav <volkov.slava@hotmail.com>
2021-12-23 11:41:36 +0300

    feat: add styles

--- react-todo/src/Todo/TodoItem.js
+++ react-todo/src/Todo/TodoItem.js
@@ -1,9 +1,32 @@
 import React from "react";
 import PropTypes from 'prop-types'
 
+const styles = {
+  li: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '.5rem 1rem',
+    border: '1px solid #ccc',
+    borderRadius: '4px',
+    marginBottom: '.5rem'
+  },
+  input: {
+    marginRight: '1rem'
+  }
+}
+
 function TodoItem({ todo, index }) {
   return (
-    <li><strong>{index + 1}</strong>{todo.title}</li>
+    <li style={styles.li}>
+      <span>
+        <input type="checkbox" style={styles.input} />
+        <strong>{index + 1}</strong>
+        &nbsp;
+        {todo.title}
+      </span>
+      <button className="rm">&times;</button>
+    </li>
   )
 }
 
