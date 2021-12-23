627eaeac120267c90f02d8d26db7e6574589bf2a
Volkov Viacheslav <volkov.slava@hotmail.com>
2021-12-23 12:54:54 +0300

    feat: add retrieve data from the server

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import TodoList from "./Todo/TodoList";
 import AddTodo from "./Todo/AddTodo";
 import Context from "./context";
 
 function App() {
 
-  const [todos, setTodos] = React.useState(
-    [
-      {id: 1, completed: false, title: 'Купить хлеб'},
-      {id: 2, completed: false, title: 'Купить масло'},
-      {id: 3, completed: false, title: 'Купить молоко'}
-    ]
-  )
+  const [todos, setTodos] = React.useState([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/?_limit=5')
+      .then(response => response.json())
+      .then(todos => {
+        setTodos(todos)
+      })
+  }, [])
 
   function toggleTodo(id) {
     setTodos(
