3fe8fa9af65d7fef109a40451d6f6aa3a42aeb24
Volkov Viacheslav <volkov.slava@hotmail.com>
2021-12-23 11:58:04 +0300

    feat: add work with state

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -3,19 +3,23 @@ import TodoList from "./Todo/TodoList";
 
 function App() {
 
-  let todos = [
-    {id: 1, completed: false, title: 'Купить хлеб'},
-    {id: 2, completed: false, title: 'Купить масло'},
-    {id: 3, completed: false, title: 'Купить молоко'}
-  ]
+  const [todos, setTodos] = React.useState(
+    [
+      {id: 1, completed: false, title: 'Купить хлеб'},
+      {id: 2, completed: false, title: 'Купить масло'},
+      {id: 3, completed: false, title: 'Купить молоко'}
+    ]
+  )
 
   function toggleTodo(id) {
-    todos = todos.map(todo => {
+    setTodos(
+      todos.map(todo => {
       if (todo.id === id) {
         todo.completed = !todo.completed
       }
       return todo;
-    })
+      })
+    )
   }
 
   return (
