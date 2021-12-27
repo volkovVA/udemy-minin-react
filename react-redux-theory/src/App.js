b5084e9c7bdc63217a157aaa8bd1531e4b1978be
Volkov Viacheslav <volkov.slava@hotmail.com>
2021-12-27 15:37:29 +0300

    feat: add react-redux

--- react-redux-theory/src/App.js
+++ react-redux-theory/src/App.js
@@ -1,31 +1,42 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import './App.scss'
 
 class App extends Component {
-  state = {
-    counter: 0
-  }
-
-  updateCounter(value) {
-    this.setState({
-      counter: this.state.counter + value
-    })
-  }
 
   render() {
     return (
       <div className={'App'}>
-        <h1>Счетчик <strong>{this.state.counter}</strong></h1>
+        <h1>Счетчик <strong>{this.props.counter}</strong></h1>
 
         <hr/>
 
         <div className="Actions">
-          <button onClick={() => this.updateCounter(1)}>Добавить 1</button>
-          <button onClick={() => this.updateCounter(-1)}>Вычесть 1</button>
+          <button onClick={this.props.onAdd}>Добавить 1</button>
+          <button onClick={this.props.onSub}>Вычесть 1</button>
+        </div>
+
+        <div className="Actions">
+          <button onClick={() => this.props.onAddNumber(15)}>Добавить</button>
+          <button onClick={() => this.props.onAddNumber(-17)}>Вычесть</button>
         </div>
       </div>
     )
   }
 }
 
-export default App
+function mapStateToProps(state) {
+  return {
+    counter: state.counter
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onAdd: () => dispatch({type: 'ADD'}),
+    onSub: () => dispatch({type: 'SUB'}),
+    onAddNumber: number => dispatch({type: 'ADD_NUMBER', payload: number})
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- react-redux-theory/src/index.js
+++ react-redux-theory/src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import rootReducer from './redux/rootReducer';
 
+const store = createStore(rootReducer);
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
