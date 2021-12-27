4bc975d1aafb32d018da36ed3ed8bef4c3fe8a61
Volkov Viacheslav <volkov.slava@hotmail.com>
2021-12-27 19:38:48 +0300

    feat: add devtools

--- react-redux-theory/src/index.js
+++ react-redux-theory/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux';
 import reduxThunk from 'redux-thunk';
 import rootReducer from './redux/rootReducer';
@@ -18,13 +18,20 @@ import rootReducer from './redux/rootReducer';
 //   }
 // }
 
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
 const loggerMiddleware = store => next => action => {
   const result = next(action);
   console.log('Middleware', store.getState());
   return result;
 }
 
-const store = createStore(rootReducer, applyMiddleware(loggerMiddleware, reduxThunk));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(loggerMiddleware, reduxThunk)));
 const app = (
   <Provider store={store}>
     <App />
