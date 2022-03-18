e5aa42405d926742ac08a9ddc6fb3dda67da385d
volkovVA <volkov.slava@hotmail.com>
2022-03-18 18:52:36 +0300

    feat: add custom hook

--- react-hooks-theory/src/App.js
+++ react-hooks-theory/src/App.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import Main from './examples/Main';
-import Alert from './examples/alert/Alert';
-import { AlertProvider } from './examples/alert/AlertContext'
+import React, { useEffect, useState } from "react";
+
+function useLogger(value) {
+  useEffect(() => {
+    console.log("Value changed", value);
+  }, [value]);
+}
+
+function useInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const clear = () => setValue("");
+
+  return {
+    bind: { value, onChange },
+    clear,
+  };
+}
 
 function App() {
+  const input = useInput("");
+
+  useLogger(input.value);
 
   return (
-    <AlertProvider>
-      <div className={'container'}>
-        <Alert />
-        <Main toggle={() => {}} />
-      </div>
-    </AlertProvider>
+    <div className="container pt-3">
+      <input
+        type="text"
+        // value={input.value}
+        // onChange={input.onChange}
+        {...input.bind}
+        className="m-1"
+      />
+      <button className="btn btn-success" onClick={() => input.clear()}>
+        Очистить
+      </button>
+      <hr />
+      <h1>{input.bind.value}</h1>
+    </div>
   );
 }
 
